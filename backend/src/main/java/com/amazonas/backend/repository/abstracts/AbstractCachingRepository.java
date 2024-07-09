0f2ada95ecee191ad4d43a8b93bc151fb28653cd
Yuval Roth <Yuval-Roth@users.noreply.github.com>
2024-07-09 13:55:53 +0300

    Added an option to disable cache

--- backend/src/main/java/com/amazonas/backend/repository/abstracts/AbstractCachingRepository.java
+++ backend/src/main/java/com/amazonas/backend/repository/abstracts/AbstractCachingRepository.java
@@ -10,36 +10,46 @@ public abstract class AbstractCachingRepository <T extends HasId<String>> {
 
     private final Map<String, T> cache;
     private final CrudCollection<T> repo;
+    private boolean cacheEnabled;
 
     public AbstractCachingRepository(CrudCollection<T> repo) {
         this.cache = new HashMap<>();
         this.repo = repo;
+        this.cacheEnabled = true;
     }
 
     public <S extends T> S save(S entity) {
         S saved = repo.save(entity);
-        cache.put(entity.getId(), entity);
+        if(cacheEnabled){
+            cache.put(entity.getId(), entity);
+        }
         return saved;
     }
 
     public void deleteAll(Iterable<? extends T> entities) {
         repo.deleteAll(entities);
-        cache.clear();
+        if(cacheEnabled){
+            cache.clear();
+        }
     }
 
     public Iterable<T> findAllById(Iterable<String> strings) {
         Iterable<T> allById = repo.findAllById(strings);
-        allById.forEach(entity -> cache.put(entity.getId(), entity));
+        if(cacheEnabled){
+            allById.forEach(entity -> cache.put(entity.getId(), entity));
+        }
         return allById;
     }
 
     public void delete(T entity) {
         repo.delete(entity);
-        cache.remove(entity.getId());
+        if(cacheEnabled){
+            cache.remove(entity.getId());
+        }
     }
 
     public Optional<T> findById(String s) {
-        if (cache.containsKey(s)) {
+        if (cacheEnabled && cache.containsKey(s)) {
             return Optional.of(cache.get(s));
         }
         return repo.findById(s);
@@ -47,36 +57,59 @@ public abstract class AbstractCachingRepository <T extends HasId<String>> {
 
     public void deleteById(String s) {
         repo.deleteById(s);
-        cache.remove(s);
+        if(cacheEnabled){
+            cache.remove(s);
+        }
     }
 
     public void deleteAll() {
         repo.deleteAll();
-        cache.clear();
+        if(cacheEnabled){
+            cache.clear();
+        }
     }
 
     public Iterable<T> findAll() {
         Iterable<T> all = repo.findAll();
-        all.forEach(entity -> cache.put(entity.getId(), entity));
+        if(cacheEnabled){
+            all.forEach(entity -> cache.put(entity.getId(), entity));
+        }
         return all;
     }
 
     public void deleteAllById(Iterable<? extends String> strings) {
         repo.deleteAllById(strings);
-        strings.forEach(cache::remove);
+        if(cacheEnabled){
+            strings.forEach(cache::remove);
+        }
     }
 
     public <S extends T> Iterable<S> saveAll(Iterable<S> entities) {
         Iterable<S> saved = repo.saveAll(entities);
-        saved.forEach(entity -> cache.put(entity.getId(), entity));
+        if(cacheEnabled){
+            saved.forEach(entity -> cache.put(entity.getId(), entity));
+        }
         return saved;
     }
 
     public boolean existsById(String s) {
-        return cache.containsKey(s) || repo.existsById(s);
+        return (cacheEnabled && cache.containsKey(s)) || repo.existsById(s);
     }
 
     public long count() {
         return repo.count();
+    }
+
+    public void clearCache() {
+        if(cacheEnabled){
+            cache.clear();
+        }
+    }
+
+    public void setCacheEnabled(boolean value) {
+        cacheEnabled = value;
+        if(!cacheEnabled) {
+            clearCache();
+        }
     }
 }
