69ae5ef9abe0b421075b13ae3671ec73daa9c22b
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-07-10 16:53:42 +0300

    cleanup

--- backend/src/main/java/com/amazonas/backend/repository/UserRepository.java
+++ backend/src/main/java/com/amazonas/backend/repository/UserRepository.java
@@ -14,49 +14,7 @@ import java.util.Map;
 @Component("userRepository")
 public class UserRepository extends AbstractCachingRepository<RegisteredUser> {
 
-//    private final Map<String, User> userCache;
-//    private final ReadWriteLock userLock;
-//
     public UserRepository(UserCrudCollection repo) {
         super(repo);
-//        userLock = new ReadWriteLock();
-//        userCache = new HashMap<>();
     }
-
-//    public User getUser(String userId) {
-//        userLock.acquireRead();
-//        try {
-//            return userCache.get(userId);
-//        } finally {
-//            userLock.releaseRead();
-//        }
-//    }
-//
-//    public void saveUser(User user) {
-//        userLock.acquireWrite();
-//        try {
-//            userCache.put(user.getUserId(), user);
-//        } finally {
-//            userLock.releaseWrite();
-//        }
-//    }
-//
-//    public void saveAllUsers(Collection<User> users) {
-//        userLock.acquireWrite();
-//        try {
-//            users.forEach(user -> userCache.put(user.getUserId(), user));
-//        } finally {
-//            userLock.releaseWrite();
-//        }
-//    }
-//
-//    public boolean userIdExists(String userId) {
-//        userLock.acquireRead();
-//        try {
-//            return userCache.containsKey(userId);
-//        } finally {
-//            userLock.releaseRead();
-//        }
-//    }
-
 }
--- backend/src/main/java/com/amazonas/backend/repository/crudCollections/NotificationCrudCollection.java
+++ backend/src/main/java/com/amazonas/backend/repository/crudCollections/NotificationCrudCollection.java
@@ -2,6 +2,8 @@ package com.amazonas.backend.repository.crudCollections;
 
 import com.amazonas.common.dtos.Notification;
 import com.amazonas.backend.repository.abstracts.CrudCollection;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationCrudCollection extends CrudCollection<Notification> {
 }
