7a007f0cde391661cad120b13cf6729bb77ae08c
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-07-14 23:25:41 +0300

    clean up

--- backend/src/main/java/com/amazonas/backend/repository/ShoppingCartRepository.java
+++ backend/src/main/java/com/amazonas/backend/repository/ShoppingCartRepository.java
@@ -15,27 +15,19 @@ public class ShoppingCartRepository {
     private final ShoppingCartFactory factory;
     private final UserRepository userRepository;
 
-    private final Map<String,ShoppingCart> cache;
-
     public ShoppingCartRepository(StoreBasketRepository storeBasketRepository,
                                   ShoppingCartFactory shoppingCartFactory, UserRepository userRepository) {
         this.repo = storeBasketRepository;
         this.factory = shoppingCartFactory;
         this.userRepository = userRepository;
-        cache = new HashMap<>();
     }
 
     public Optional<ShoppingCart> findById(String userId) {
-//        if (cache.containsKey(userId)) {
-//            return Optional.of(cache.get(userId));
-//        }
-
         if(! userRepository.existsById(userId)) {
             return Optional.empty();
         }
         ShoppingCart shoppingCart = factory.get(userId);
         shoppingCart.baskets().addAll(repo.findStoreIdsByUserId(userId));
-//        cache.put(userId, shoppingCart);
         return Optional.of(shoppingCart);
     }
 
