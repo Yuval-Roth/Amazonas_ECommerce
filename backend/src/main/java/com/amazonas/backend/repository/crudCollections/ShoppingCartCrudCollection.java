ea21606c57f37bf045ac51ae7110b34a8c69e61c
Yuval Roth <Yuval-Roth@users.noreply.github.com>
2024-07-14 18:04:24 +0300

    removed shopping cart crud collection

--- backend/src/main/java/com/amazonas/backend/repository/crudCollections/ShoppingCartCrudCollection.java
+++
@@ -1,9 +0,0 @@
-package com.amazonas.backend.repository.crudCollections;
-
-import com.amazonas.backend.business.userProfiles.ShoppingCart;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShoppingCartCrudCollection extends CrudRepository<ShoppingCart, String>{
-}
